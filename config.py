b19c200a140a64760383847963faeec8a117a9b5
UNG MEI SHAN <113978307+UNG-MEI-SHAN@users.noreply.github.com>
2024-04-17 21:31:06 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cfg4yo682lz3.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "ungmeishan-employee"
+customregion = "us-east-1"
+
